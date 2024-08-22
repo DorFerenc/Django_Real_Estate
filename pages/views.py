653ecd7279c19a422b2fc4fc05edf0211fe510b0
DorFerenc <dor.ferenc@s.afeka.ac.il>
2024-08-23 01:14:32 +0300

    added db access to home and about pages

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+from realtors.models import Realtor
+
 # Create your views here.
 def index(request):
     # return HttpResponse('<h1>Hello World</h1>')
-    return render(request, 'pages/index.html')
+    listings_from_db = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings_from_db
+    }
+
+    return render(request, 'pages/index.html', context)
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # Get all realtors
+    realtors_from_db = Realtor.objects.order_by('-hire_date')
+    # Get mvp
+    mvp_realtors_from_db = Realtor.objects.all().filter(is_mvp=True)
+
+    context = {
+        'realtors': realtors_from_db,
+        'mvp_realtors': mvp_realtors_from_db
+    }
+    return render(request, 'pages/about.html', context)
