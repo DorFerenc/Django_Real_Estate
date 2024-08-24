ed5949b63c544789b0bcae08589cdb43299b8da1
DorFerenc <dor.ferenc@s.afeka.ac.il>
2024-08-24 16:41:58 +0300

    finished basic features

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 # Create your views here.
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
 
 def register(request):
     if request.method == 'POST':
@@ -79,4 +80,10 @@ def logout(request):
 
 
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+
+    context = {
+        'contacts': user_contacts
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
